1b12f60e554a451917831bd8298ff149281bab83
EArezki <arezki.elmehdi@gmail.com>
2019-05-31 22:52:13 +0200

    add test for fixed price per unit

---
+++ src/test/java/com/sgcib/CheckoutTest.java
@@ -0,0 +1,25 @@
+package com.sgcib;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+public class CheckoutTest {
+
+    /**
+     * This can of beans costs $0.65
+     */
+    @Test
+    public void test_fixed_price() {
+        List<Product> products = asList(new Product("Can of beans", new Price(new BigDecimal("0.6")), new Quantity(1)));
+        Cart cart = new Cart(products);
+        BigDecimal totalPrice = cart.totalPrice();
+        assertEquals(new BigDecimal("0.6"), totalPrice);
+    }
+
+
+}
